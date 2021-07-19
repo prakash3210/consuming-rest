38acc263169d6260aefabc709cf9dba969b32c71
prakashsharma <prakash3210@gmail.com>
2021-07-19 18:39:17 +0530

    C1

--- src/main/java/com/example/consuming/rest/ConsumingRestApplication.java
+++ src/main/java/com/example/consuming/rest/ConsumingRestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConsumingRestApplication {
 
 	public static void main(String[] args) {
+		//Comment
 		SpringApplication.run(ConsumingRestApplication.class, args);
 	}
 
